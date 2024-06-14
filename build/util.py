3b11876d8744387f01ae2093a170e597dc26509c
Aeldrion <aeldrion@gmail.com>
2024-06-14 11:49:39 +0200

    Print less information when building

--- build/util.py
+++ build/util.py
@@ -1,18 +1,23 @@
 from collections import defaultdict
 import json, os
 
+
 def shape_to_snbt(s: str) -> str:
     """Converts a shape of the form [AABB[x, y, z] -> [x, y, z]] into an SNBT string of the form [{min: [x, y, z], max: [x, y, z]}]"""
-    bounding_boxes = eval(s
-                            .replace("AABB", '(')
-                            .replace(', (', '), (')
-                            .replace(' ->', ',')
-                            .replace(']]', '])]')
-                        )
-    snbt_string = ','.join([
-        f"{{min: {min_corner}, max: {max_corner}}}" for min_corner, max_corner in bounding_boxes
-    ])
+    bounding_boxes = eval(
+        s.replace("AABB", "(")
+        .replace(", (", "), (")
+        .replace(" ->", ",")
+        .replace("]]", "])]")
+    )
+    snbt_string = ",".join(
+        [
+            f"{{min: {min_corner}, max: {max_corner}}}"
+            for min_corner, max_corner in bounding_boxes
+        ]
+    )
     return f"[{snbt_string}]"
+
 
 def group_dict_keys(d: dict) -> list[list]:
     """
@@ -27,11 +32,13 @@ def group_dict_keys(d: dict) -> list[list]:
 
     return [sorted(group) for group in groups.values()]
 
+
 def unnamespace(id_: str) -> str:
     """Remove the namespace from a Minecraft namespaced ID"""
     return id_.split(":")[1]
 
-def make_tag(values: list[str], path: str, name = None, required = True) -> None:
+
+def make_tag(values: list[str], path: str, name=None, required=True) -> None:
     """
     Creates a tag file at the given path with the given values
     If no name is provided, the first value is used for the tag name
@@ -39,35 +46,27 @@ def make_tag(values: list[str], path: str, name = None, required = True) -> None
 
     if name is None:
         name = unnamespace(values[0])
-    print(f"Creating tag {name} in {path}")
-    
-    contents = [{
-        "id": id_,
-        "required": False
-    } for id_ in values]
+
+    contents = [{"id": id_, "required": False} for id_ in values]
 
     os.makedirs(path, exist_ok=True)
-    with open(f"{path}/{name}.json", mode='w') as tag_file:
+    with open(f"{path}/{name}.json", mode="w") as tag_file:
         if required:
             json.dump({"values": contents}, tag_file, indent=4)
         else:
-            json.dump({
-                "values": [
-                    {
-                        "id": id_,
-                        "required": False
-                    } for id_ in values
-                ]
-            }, tag_file, indent=4)
+            json.dump(
+                {"values": [{"id": id_, "required": False} for id_ in values]},
+                tag_file,
+                indent=4,
+            )
+
 
 def make_function(commands: list[str], path: str, name: str, header: str = "") -> None:
     """Creates a fucntion file at the given path with the given commands"""
 
-    print(f"Creating function {name} in {path}")
-
     os.makedirs(path, exist_ok=True)
-    with open(f"{path}/{name}.mcfunction", mode='w') as function_file:
+    with open(f"{path}/{name}.mcfunction", mode="w") as function_file:
         if header:
-            function_file.write(header + '\n')
+            function_file.write(header + "\n")
         for command in commands:
-            function_file.write(command + '\n')
+            function_file.write(command + "\n")
